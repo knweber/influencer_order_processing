b47967d594a4f564a72343455c5cb035c40681ce
Ryan Barth <ryan@barth.tech>
2018-01-04 19:24:30 -0800

    provide default environment

--- lib/init.rb
+++ lib/init.rb
@@ -1,5 +1,5 @@
 require 'bundler'
-Bundler.require(:default, ENV['RACK_ENV'])
+Bundler.require(:default, ENV['RACK_ENV'] || :development)
 Dotenv.load
 Dir[File.dirname(__FILE__) + '/../lib/**/*.rb'].each do |file|
   require file
